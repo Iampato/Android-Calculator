9fcf80e21aeeac16e9db4f34b5b2e308965d8e8b
Iampato <858wpwaweru@gmail.com>
2020-06-28 11:38:57 +0300

    added expression library

--- app/src/main/java/tech/ally/calculator/MainActivity.kt
+++ app/src/main/java/tech/ally/calculator/MainActivity.kt
@@ -2,7 +2,12 @@ package tech.ally.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import net.objecthunter.exp4j.ExpressionBuilder
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     //    lateinit var results:TextView
@@ -13,74 +18,86 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun button7Clicked() {
+    fun button7Clicked(view: View) {
         appendOnExpression("7", true)
     }
 
-    fun button8Clicked() {
+    fun button8Clicked(view: View) {
         appendOnExpression("8", true)
     }
 
-    fun button9Clicked() {
+    fun button9Clicked(view: View) {
         appendOnExpression("9", true)
     }
 
-    fun button0Clicked() {
+    fun button0Clicked(view: View) {
         appendOnExpression("0", true)
     }
 
-    fun button4Clicked() {
+    fun button4Clicked(view: View) {
         appendOnExpression("4", true)
     }
 
-    fun button3Clicked() {
+    fun button3Clicked(view: View) {
         appendOnExpression("3", true)
     }
 
-    fun button2Clicked() {
+    fun button2Clicked(view: View) {
         appendOnExpression("2", true)
     }
 
-    fun button1Clicked() {
+    fun button1Clicked(view: View) {
         appendOnExpression("1", true)
     }
 
-    fun button6Clicked() {
+    fun button6Clicked(view: View) {
         appendOnExpression("6", true)
     }
 
-    fun button5Clicked() {
+    fun button5Clicked(view: View) {
         appendOnExpression("5", true)
     }
 
 
-    fun buttonDivideClicked() {
+    fun buttonDivideClicked(view: View) {
         appendOnExpression("/", false)
     }
 
-    fun buttonMultiplyClicked() {
+    fun buttonMultiplyClicked(view: View) {
         appendOnExpression("*", false)
     }
 
-    fun buttonAddClicked() {
+    fun buttonAddClicked(view: View) {
         appendOnExpression("+", false)
     }
 
-    fun buttonCancelClicked() {
+    fun buttonCancelClicked(view: View) {
         textView.text = ""
         textView3.text = ""
     }
 
-    fun buttonDotClicked() {
+    fun buttonDotClicked(view: View) {
         appendOnExpression(".", false)
     }
 
-    fun buttonSubtractClicked() {
+    fun buttonSubtractClicked(view: View) {
         appendOnExpression("-", false)
     }
 
-    fun buttonEqualsClicked() {
+    fun buttonEqualsClicked(view: View) {
+        try {
+            val exp = ExpressionBuilder(textView.text.toString()).build()
+            val results = exp.evaluate()
+            val longResults = results.toLong()
+            if(results == longResults.toDouble())
+                textView3.text = longResults.toString()
+            else
+                textView3.text = results.toString()
 
+        } catch (e: Exception) {
+            Toast.makeText(applicationContext,"Exception occurred",Toast.LENGTH_SHORT)
+            Log.d("Exception", "message: " + e.message)
+        }
     }
 
     private fun appendOnExpression(value: String, canClear: Boolean) {
